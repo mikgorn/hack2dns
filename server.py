2bab4f5285fd06922d81a4245d5982beb1d1dc94
mikgorn <kirik77707@gmail.com>
2020-06-20 02:28:17 +0300

    added Database class

--- server.py
+++ server.py
@@ -2,11 +2,21 @@ from typing import *
 from pathlib import Path
 
 from flask import Flask, render_template
+from sqlite3 import *
 
 app = Flask(__name__)
 
 
 _current_path: Path = Path(__file__).parent
+
+class Database:
+    def __init__(self, dbfile):
+        self.connection = connect(dbfile)
+        self.c = self.connection.cursor()
+
+    def initialize(self):
+        self.c.execute("CREATE TABLE IF NOT EXISTS users  (id text, firstname text, secondname text, patronymic text, age datetime, email text, password text )")
+        self.connection.commit()
 
 
 class Server:
@@ -24,4 +34,6 @@ class Server:
 
 
 if __name__ == "__main__":
+    db = Database("hack2.db")
+    db.initialize()
     app.run()
