6bacb482c57939cf877cdd5033723a5a7cedacb6
mikgorn <kirik77707@gmail.com>
2020-06-20 02:44:42 +0300

    db

--- server.py
+++ server.py
@@ -3,20 +3,13 @@ from pathlib import Path
 
 from flask import Flask, render_template
 from sqlite3 import *
+from database import *
 
 app = Flask(__name__)
 
 
 _current_path: Path = Path(__file__).parent
 
-class Database:
-    def __init__(self, dbfile):
-        self.connection = connect(dbfile)
-        self.c = self.connection.cursor()
-
-    def initialize(self):
-        self.c.execute("CREATE TABLE IF NOT EXISTS users  (id text, firstname text, secondname text, patronymic text, age datetime, email text, password text )")
-        self.connection.commit()
 
 
 class Server:
@@ -31,7 +24,12 @@ class Server:
     def register():
         return render_template("registration.html")
 
-
+    @staticmethod
+    @app.route("/new_user",methods=['POST','GET'])
+    def new_user():
+        error = None
+        if request.method=='POST' :
+                return 0
 
 if __name__ == "__main__":
     db = Database("hack2.db")
